c83db0e26d05d0687f5c778e8f2ff66e406a6d04
Ron Gierlach <rongierlach@yahoo.com>
2014-05-08 13:45:49 -0500

    added migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,17 @@
-require 'nokogiri'
-require 'open-uri'
+# SCRAPER
+  # require 'nokogiri'
+  # require 'open-uri'
 
+  # File.open('db/headlines.txt', 'w') do |file| 
+  #   1.upto(317) do |i|
+  #     url = "http://www.upworthy.com/page/#{i}"
+  #     puts("--"+url)
+  #     doc = Nokogiri::HTML.parse(open(url, "User-Agent"=>"Mozilla/5.0 (Windows NT 6.0; WOW64) AppleWebKit/534.24 (KHTML, like Gecko) Chrome/11.0.696.16 Safari/534.24"))
+  #     doc.css('h3 a').each do |e| 
+  #       file.puts()
+  #       file.write(e.text) 
+  #     end
+  #   end 
+  # end
+
+  
